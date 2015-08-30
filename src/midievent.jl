abba4e3530f045b0ccd8d019d2825dc7185ad056
Joel Hobson <joel.hobsn@gmail.com>
2015-08-30 13:18:35 -0400

    Moved functions to midi.jl

--- src/midievent.jl
+++ src/midievent.jl
@@ -3,3 +3,53 @@ type MIDIEvent <: TrackEvent
     status::Uint8
     data::Array{Uint8,1}
 end
+
+function isstatusbyte(b::Uint8)
+    (b & 0b10000000) == 0b10000000
+end
+
+function isdatabyte(b::Uint8)
+    !isstatusbyte(b)
+end
+
+laststatus = 0
+function readmidievent(dT::Int64, f::IOStream)
+    data = Uint8[]
+
+    statusbyte = read(f, Uint8)
+    highnybble = statusbyte & 0b11110000
+    global laststatus
+
+    toread = 0
+    if haskey(EVENTTYPETOLENGTH, highnybble)
+        laststatus = statusbyte
+        toread = EVENTTYPETOLENGTH[highnybble]
+    else # Running status is in use
+        toread = EVENTTYPETOLENGTH[laststatus & 0b11110000]
+        statusbyte = laststatus
+        skip(f, -1)
+    end
+
+    bytecount = 0
+
+    while bytecount < toread
+        b = read(f, Uint8)
+        push!(data, b)
+
+        bytecount += 1
+    end
+
+    MIDIEvent(dT, statusbyte, data)
+end
+
+function writeevent(f::IOStream, event::MIDIEvent, status::Uint8)
+    writevariablelength(f, event.dT)
+
+    if status == 0
+        write(f, event.status)
+    end
+
+    for b in event.data
+        write(f, b)
+    end
+end
--- src/util.jl
+++ src/util.jl
@@ -44,14 +44,6 @@ function writevariablelength(f::IOStream, number::Int64)
     end
 end
 
-function isstatusbyte(b::Uint8)
-    (b & 0b10000000) == 0b10000000
-end
-
-function isdatabyte(b::Uint8)
-    !isstatusbyte(b)
-end
-
 function ismetaevent(b::Uint8)
     b == 0xFF
 end
@@ -62,48 +54,6 @@ end
 
 function ismidievent(b::Uint8)
     !ismetaevent(b) && !issysexevent(b)
-end
-
-laststatus = 0
-function readmidievent(dT::Int64, f::IOStream)
-    data = Uint8[]
-
-    statusbyte = read(f, Uint8)
-    highnybble = statusbyte & 0b11110000
-    global laststatus
-
-    toread = 0
-    if haskey(EVENTTYPETOLENGTH, highnybble)
-        laststatus = statusbyte
-        toread = EVENTTYPETOLENGTH[highnybble]
-    else # Running status is in use
-        toread = EVENTTYPETOLENGTH[laststatus & 0b11110000]
-        statusbyte = laststatus
-        skip(f, -1)
-    end
-
-    bytecount = 0
-
-    while bytecount < toread
-        b = read(f, Uint8)
-        push!(data, b)
-
-        bytecount += 1
-    end
-
-    MIDIEvent(dT, statusbyte, data)
-end
-
-function writeevent(f::IOStream, event::MIDIEvent, status::Uint8)
-    writevariablelength(f, event.dT)
-
-    if status == 0
-        write(f, event.status)
-    end
-
-    for b in event.data
-        write(f, b)
-    end
 end
 
 function writeevent(f::IOStream, event::MIDIEvent)
