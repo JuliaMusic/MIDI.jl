14c9cbe3e2d35f4a464321828902b7e4564fb670
Joel Hobson <joel.hobsn@gmail.com>
2015-08-30 13:35:15 -0400

    Not sure what changed here...

--- src/miditrack.jl
+++ src/miditrack.jl
@@ -3,7 +3,6 @@ Track chunks begin with four bytes spelling out "MTrk", followed by the length
 in bytes of the track (see readvariablelength in util.jl), followed by a sequence
 of events.
 =#
-
 type MIDITrack
     events::Array{TrackEvent, 1}
     length::Uint32
