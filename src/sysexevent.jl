3c22137bf679cde519f871595128a65c645de2b9
Joel Hobson <joel.hobsn@gmail.com>
2015-09-05 14:20:41 -0400

    Fixes 10

--- src/sysexevent.jl
+++ src/sysexevent.jl
@@ -1,6 +1,5 @@
 type SysexEvent <: TrackEvent
     dT::Int
-    status::Uint8
     data::Array{Uint8,1}
 end
 
@@ -10,7 +9,8 @@ end
 
 function readsysexevent(dT::Uint8, f::IO)
     data = Uint8[]
-    statusbyte = read(f, Uint8)
+    # Read the length, but don't bother recording it since we can calculate it from the rest of the stream.
+    readvariablelength(f)
     b = read(f, Uint8)
     while isdatabyte(b)
         push!(data, b)
@@ -19,18 +19,19 @@ function readsysexevent(dT::Uint8, f::IO)
         end
         b = read(f, Uint8)
     end
-    skip(f, -1)
+    # The last byte of sysex event is F7. We leave that out of the data, and write it back when we write the event.
 
-    SysexEvent(dT, statusbyte, data)
+    SysexEvent(dT, data)
 end
 
 function writeevent(f::IO, event::SysexEvent)
-    write(f, SYSEX)
     writevariablelength(f, event.dT)
-    write(f, event.status)
+    write(f, SYSEX)
+    writevariablelength(f, length(event.data) + 1) # +1 for the ending F7
     for b in event.data
         write(f, b)
     end
+    write(f, 0xF7)
 end
 
 export SysexEvent
