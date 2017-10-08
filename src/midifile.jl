14bd7c2c7f7298920897947835dcea6014b1227a
George Datseris <datseris.george@gmail.com>
2017-10-08 13:06:16 +0200

    allow reading files without ".mid"
    Internally adds ending ".mid" if it encounters such a file.

--- src/midifile.jl
+++ src/midifile.jl
@@ -30,6 +30,9 @@ end
 Read a file into a MIDIFile data type.
 """
 function readMIDIfile(filename::AbstractString)
+    if length(f) < 4 || f[end-3:end] != ".mid"
+	f *= ".mid"
+    end
     f = open(filename)
 
     MIDIfile = MIDIFile()
