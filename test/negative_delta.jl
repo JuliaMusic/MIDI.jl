a4ea95c694578a90d1d87dd5bd902364acec1b42
Christoph Finkensiep <chfin@chfin.de>
2018-02-27 18:10:36 +0100

    add tests for writing negative deltas checks

--- test/negative_delta.jl
+++ test/negative_delta.jl
@@ -1,6 +1,16 @@
 cd(@__DIR__)
 
-@testset "negative delta" begin
+@testset "reading negative delta" begin
     midi = readMIDIfile("negative_delta.mid")
     @test midi.tracks[1].events[3].dT == -48
+end
+
+@testset "writing negative delta" begin
+    stream = IOBuffer()
+    @test_throws ErrorException MIDI.writeevent(stream, MIDIEvent(-1, 0x80, [0x00, 0x00]))
+    @test_throws ErrorException MIDI.writeevent(stream, MetaEvent(-1, 0x51, [0x00, 0x00, 0x01]))
+    @test_throws ErrorException MIDI.writeevent(stream, SysexEvent(-1, [0x01]))
+    @test_throws ErrorException MIDI.writevariablelength(stream, -1)
+    @test_throws ErrorException MIDI.writevariablelength(stream, Int(0x1FFFFFFF))
+    close(stream)
 end
