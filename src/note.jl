c59c6f49ee8d567f2efbcfc1b53279cf32416d3e
George Datseris <datseris.george@gmail.com>
2018-08-12 16:29:09 +0100

    add copy(notes)

--- src/note.jl
+++ src/note.jl
@@ -98,6 +98,7 @@ function Base.append!(n1::Notes{N}, n2::Notes{N}) where {N}
     return n1
 end
 
+Base.copy(notes::Notes) = Notes(copy(notes.notes), notes.tpq)
 # Pretty printing
 const PITCH_TO_NAME = Dict(
 0=>"C", 1=>"C♯", 2=>"D", 3=>"D♯", 4=>"E", 5=>"F", 6=>"F♯", 7=>"G", 8=>"G♯", 9=>"A",
