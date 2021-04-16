92095b36c62bea30f3d004c512e917cf065a62ee
vasanth <vasanth.mani.vasi.2@gmail.com>
2021-04-16 21:41:54 +0530

    Add tests

--- test/utils.jl
+++ test/utils.jl
@@ -3,6 +3,10 @@
     @test time_signature(midi) == "4/4"
     @test qpm(midi) ≈ 130.00013
     @test bpm(midi) ≈ 130.00013
+
+    first_tempo_event = get_tempo_changes(midi)[1]
+    @test first_tempo_event[1] == 0
+    @test first_tempo_event[2] ≈ 130.00013
 end
 
 
