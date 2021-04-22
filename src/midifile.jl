432f43dc6cbe22fae9c155d5f2412b25d5e29a53
vasanth <vasanth.mani.vasi.2@gmail.com>
2021-04-22 20:04:09 +0530

    Add load and save methods

--- src/midifile.jl
+++ src/midifile.jl
@@ -1,5 +1,7 @@
-export MIDIFile, readMIDIFile, writeMIDIFile
+export MIDIFile
 export BPM, bpm, qpm, time_signature, tempochanges, ms_per_tick
+
+using FileIO
 
 """
     MIDIFile <: Any
@@ -33,81 +35,65 @@ end
 
 MIDIFile() = MIDIFile(0,960,MIDITrack[])
 
-function readMIDIFileastype0(filename::AbstractString)
-	MIDIFile = readMIDIFile(filename)
-	if MIDIFile.format == 1
-		type1totype0!(MIDIFile)
-	end
-	MIDIFile
-end
-
 """
-    readMIDIFile(filename::AbstractString)
+    load(filename::File{format"MIDI"})
 Read a file into a `MIDIFile` data type.
 """
-function readMIDIFile(filename::AbstractString)
-    # Filename does not end in ".mid", ".MID", ".midi"
-    if match(r"\.(mid|MID|midi)$", filename) === nothing
-		filename *= ".mid"
+function load(f::File{format"MIDI"})
+    open(f) do s
+        skipmagic(s)
+        midifile = load(s)
     end
-    f = open(filename)
+end
 
+function load(s::Stream{format"MIDI"})
     midifile = MIDIFile()
-    # Check that it's a valid MIDI file - first four bytes should spell MThd
-    mthd = join(map(Char, read!(f, Array{UInt8}(undef, 4))))
-    if mthd != MTHD
-        error("Not a valid MIDI file. Expected first 4 bytes to spell 'MThd', got $(mthd)")
-    end
 
     # Skip the next four bytes - this is the header size, and it's always equal to 6.
-    skip(f, 4)
+    skip(s, 4)
 
     # Read the format code. 0 = single track, 1 = multiple tracks, 2 = multiple songs
     # Remember - MIDI files store data in big-endian format, which is why ntoh is used
-    midifile.format = ntoh(read(f, UInt16))
+    midifile.format = ntoh(read(s, UInt16))
 
     # Get the number of tracks and time division
-    numberoftracks = ntoh(read(f, UInt16))
-    midifile.tpq = ntoh(read(f, Int16))
-    midifile.tracks = [readtrack(f) for x in 1:numberoftracks]
-    close(f)
+    numberoftracks = ntoh(read(s, UInt16))
+    midifile.tpq = ntoh(read(s, Int16))
+    midifile.tracks = [readtrack(s.io) for x in 1:numberoftracks]
 
     midifile
 end
 
-readMIDIFile() = readMIDIFile(testmidi())
-
 """
-    writeMIDIFile(filename::AbstractString, data::MIDIFile)
+    save(filename::File{format"MIDI"}, data::MIDIFile)
 Write a `MIDIFile` as a ".mid" file to the given filename.
 
-    writeMIDIFile(filename::AbstractString, notes::Notes)
+    save(filename::File{format"MIDI"}, notes::Notes)
 Create a `MIDIFile` directly from `notes`, using format 1.
 """
-function writeMIDIFile(filename::AbstractString, data::MIDIFile)
-    if match(r"\.(mid|MID|midi)$", filename) === nothing
-      filename *= ".mid"
+function save(f::File{format"MIDI"}, data::MIDIFile)
+    open(f, "w") do s
+        save(s, data)
     end
+end
 
-    f = open(filename, "w")
+function save(s::Stream{format"MIDI"}, data::MIDIFile)
+    write(s, magic(format"MIDI"))
 
-    write(f, MTHD) # File identifier
-    write(f, hton(convert(UInt32, 6))) # Header length
-    write(f, hton(data.format))
-    write(f, hton(convert(UInt16, length(data.tracks))))
-    write(f, hton(data.tpq))
+    write(s, hton(convert(UInt32, 6))) # Header length
+    write(s, hton(data.format))
+    write(s, hton(convert(UInt16, length(data.tracks))))
+    write(s, hton(data.tpq))
 
-    map(track->writetrack(f, track), data.tracks)
-
-    close(f)
+    map(track->writetrack(s.io, track), data.tracks)
     return data
 end
 
-function writeMIDIFile(filename::AbstractString, notes::Notes)
+function save(s::Union{File{format"MIDI"}, Stream{format"MIDI"}}, notes::Notes)
     track = MIDITrack()
     addnotes!(track, notes)
     midi = MIDIFile(1, notes.tpq, [track])
-    writeMIDIFile(filename, midi)
+    save(s, midi)
     return midi
 end
 
