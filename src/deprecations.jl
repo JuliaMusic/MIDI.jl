da1d32156790533382af08a0a683ebc1bc4cf8f3
vasanth <vasanth.mani.vasi.2@gmail.com>
2021-04-24 23:54:05 +0530

    Fix deprecation error

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,3 +1,3 @@
-@deprecate writeMIDIFile(filename::AbstractString, data::MIDIFile) MIDI.save(filename, data)
-@deprecate writeMIDIFile(filename::AbstractString, notes::Notes) MIDI.save(filename, notes)
-@deprecate readMIDIFile(filename::AbstractString) MIDI.load(filename)
+@deprecate writeMIDIFile(filename::AbstractString, data::MIDIFile) save(File{format"MIDI"}(filename), data)
+@deprecate writeMIDIFile(filename::AbstractString, notes::Notes) save(File{format"MIDI"}(filename), notes)
+@deprecate readMIDIFile(filename::AbstractString) load(File{format"MIDI"}(filename))
