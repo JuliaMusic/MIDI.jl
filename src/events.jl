432c2c79a8fbed96c57ed21931fc42cf98d663f0
Vasanth <vasanth.mani.vasi.2@gmail.com>
2021-06-12 03:19:57 +0530

    Add type support for MIDIEvents

--- src/events.jl
+++ src/events.jl
@@ -2,9 +2,10 @@ export encode, decode
 
 # Define the spec which maps from type byte to the type definitions
 const spec = Dict(
+    # MetaEvents
     0x00 => (
         type = :SequenceNumber,
-        fields = [(:number, Int)],
+        fields = ["number::Int"],
         decode = :(ntoh.(reinterpret(UInt16, data))),
         encode = :(UInt8.([event.number >> 8 & 0xFF, event.number & 0xFF]))
     ),
@@ -17,7 +18,7 @@ const spec = Dict(
     0x07 => :CuePoint,
     0x20 => (
         type = :MIDIChannelPrefix,
-        fields = [(:channel, Int)],
+        fields = ["channel::Int"],
         decode = :(Int.(data)),
         encode = :(UInt8(event.channel))
     ),
@@ -28,29 +29,73 @@ const spec = Dict(
         encode = :(UInt8[])
     ),
     0x51 => (
-        type = :SetTempo, 
-        fields = [(:tempo, Int)],
+        type = :SetTempo,
+        fields = ["tempo::Int"],
         decode = :(ntoh.(reinterpret(UInt32, pushfirst!(data, 0x00)))),
         encode = :(UInt8.([event.tempo >> 16, event.tempo >> 8 & 0xFF, event.tempo & 0xFF]))
     ),
     # TODO: Add SMPTEOffset
     0x58 => (
-        type = :TimeSignature, 
-        fields = [(:numerator, Int), (:denominator, Int), (:clockticks, Int), (:notated32nd_notes, Int)],
+        type = :TimeSignature,
+        fields = ["numerator::Int", "denominator::Int", "clockticks::Int", "notated32nd_notes::Int"],
         decode = :(Int.([data[1], 2^data[2], data[3:4]...])),
         encode = :(UInt8.([event.numerator, log2(event.denominator), event.clockticks, event.notated32nd_notes]))
     ),
     0x59 => (
-        type = :KeySignature, 
-        fields = [(:sf, Int), (:mi, Int)],
-        decode = :(Int.(data[1:2])),
+        type = :KeySignature,
+        fields = ["sf::Int", "mi::Int"],
+        decode = :(Int.(data)),
         encode = :(UInt8.([event.sf, event.mi]))
+    ),
+
+    # MidiEvents
+    0x80 => (
+        type = :NoteOff,
+        fields = ["note::Int", "velocity::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.note, event.velocity]))
+    ),
+    0x90 => (
+        type = :NoteOn,
+        fields = ["note::Int", "velocity::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.note, event.velocity]))
+    ),
+    0xA0 => (
+        type = :Aftertouch,
+        fields = ["note::Int", "pressure::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.note, event.pressure]))
+    ),
+    0xB0 => (
+        type = :ControlChange,
+        fields = ["controller::Int", "value::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.controller, event.value]))
+    ),
+    0xC0 => (
+        type = :ProgramChange,
+        fields = ["program::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.program]))
+    ),
+    0xD0 => (
+        type = :ChannelPressure,
+        fields = ["pressure::Int"],
+        decode = :(Int.(data)),
+        encode = :(UInt8.([event.pressure]))
+    ),
+    0xE0 => (
+        type = :PitchBend,
+        fields = ["pitch::Int"],
+        decode = :(Int.((ntoh(reinterpret(UInt16, [0x00, data[2]])[1]) << 8 | data[1] << 1) >> 1)),
+        encode = :(UInt8.([event.pitch & 0b01111111, event.pitch >> 7]))
     ),
 )
 
 # Store the defs for text-only events separately to avoid repetition
 text_defs = (
-    fields = [(:text, String)],
+    fields = ["text::String"],
     decode = :([join(Char.(data))]),
     encode = :(UInt8.(collect(event.text)))
 )
@@ -59,9 +104,9 @@ text_defs = (
 const type2byte = Dict((value isa Symbol ? value : value.type) => key for (key, value) in spec)
 
 # Define a struct, constructor and encode function for a given type
-function define_type(type, fields, decode, encode_)
+function define_type(type, fields, decode, encode_, supertype)
     @eval begin
-        mutable struct $(type) <: MetaEvent
+        mutable struct $(type) <: $supertype
             dT::Int
             $(fields...)
         end
@@ -88,6 +133,15 @@ for defs in values(spec)
     else
         type, fields, decode, encode = defs
     end
-    fields = [:($(fieldname)::$(fieldtype)) for (fieldname, fieldtype) in fields]
-    define_type(type, fields, decode, encode)
+
+    typebyte = type2byte[type]
+    if 0x80 <= typebyte <= 0xEF
+        supertype = MIDIEvent
+        pushfirst!(fields, "channel::Int")
+    else
+        supertype = MetaEvent
+    end
+
+    fields = Meta.parse.(fields)
+    define_type(type, fields, decode, encode, supertype)
 end
